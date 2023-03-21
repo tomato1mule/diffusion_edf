02e5d2480e3480dae0975a21beb360c04395f621
Hyunwoo Ryu <tomato1mule@gmail.com>
2023-03-21 20:23:50 +0900

    working with equiformer

---
+++ diffusion_edf/equiformer/__init__.py
@@ -0,0 +1,5 @@
+from .registry import model_entrypoint
+
+from .graph_attention_transformer import *
+from .dp_attention_transformer import *
+
---
+++ diffusion_edf/equiformer/bessel_rbf.py
@@ -0,0 +1,8 @@
+import torch
+
+class RadialBasis(torch.nn.Module):
+    def __init__(self, *args, **kwargs):
+        raise NotImplementedError
+    
+    def forward(self, *args, **kwargs):
+        raise NotImplementedError
