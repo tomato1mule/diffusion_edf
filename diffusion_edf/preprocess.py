8102ac610ba3caea180f2b06a5114f1a587c6627
Hyunwoo Ryu <tomato1mule@gmail.com>
2023-03-24 19:50:35 +0900

    equivariance check

---
+++ diffusion_edf/preprocess.py
@@ -0,0 +1,234 @@
+from typing import Union, Optional, List, Tuple, Dict
+
+import numpy as np
+
+import torch
+
+from diffusion_edf.data import PointCloud, SE3, TargetPoseDemo, DemoSequence
+from diffusion_edf.pc_utils import voxel_filter, normalize_pc_color
+
+
+def rescale(data, rescale_factor: float):
+    if data is None:
+        return None
+    elif type(data) == DemoSequence:
+        demo_seq = [rescale(demo, rescale_factor=rescale_factor) for demo in data]
+        return DemoSequence(demo_seq = demo_seq, device=data.device)
+    elif type(data) == TargetPoseDemo:
+        scene_pc = rescale(data.scene_pc, rescale_factor=rescale_factor)
+        grasp_pc = rescale(data.grasp_pc, rescale_factor=rescale_factor)
+        target_poses = rescale(data.target_poses, rescale_factor=rescale_factor)
+        return TargetPoseDemo(scene_pc=scene_pc, grasp_pc=grasp_pc, target_poses=target_poses, device=scene_pc.device)
+    elif type(data) == PointCloud:
+        if data.is_empty():
+            return data
+        else:
+            points = data.points * rescale_factor
+            return PointCloud(points=points, colors=data.colors.clone(), device=data.device)
+    elif type(data) == SE3:
+        poses = data.poses * torch.tensor([1., 1., 1., 1., rescale_factor, rescale_factor, rescale_factor], dtype=data.poses.dtype, device=data.poses.device).unsqueeze(-2)
+        return SE3(poses=poses, device=data.device)
+    else:
+        raise TypeError(f"Unknown data type ({type(data)}) is given.")
+
+
+
+def normalize_color(data, color_mean: torch.Tensor, color_std: torch.Tensor):
+    if data is None:
+        return None
+    elif type(data) == DemoSequence:
+        demo_seq = [normalize_color(demo, color_mean=color_mean, color_std=color_std) for demo in data]
+        return DemoSequence(demo_seq = demo_seq, device=data.device)
+    elif type(data) == TargetPoseDemo:
+        scene_pc = normalize_color(data.scene_pc, color_mean=color_mean, color_std=color_std)
+        grasp_pc = normalize_color(data.grasp_pc, color_mean=color_mean, color_std=color_std)
+        target_poses = normalize_color(data.target_poses, color_mean=color_mean, color_std=color_std)
+        return TargetPoseDemo(scene_pc=scene_pc, grasp_pc=grasp_pc, target_poses=target_poses, device=data.device)
+    elif type(data) == PointCloud:
+        if data.is_empty():
+            return data
+        else:
+            return normalize_pc_color(data=data, color_mean=color_mean, color_std=color_std)
+    elif type(data) == SE3:
+        return data
+    else:
+        raise TypeError(f"Unknown data type ({type(data)}) is given.")
+
+
+
+def downsample(data, voxel_size: float, coord_reduction: str = "average"):
+    if data is None:
+        return None
+    elif type(data) == DemoSequence:
+        demo_seq = [downsample(demo, voxel_size=voxel_size, coord_reduction=coord_reduction) for demo in data]
+        return DemoSequence(demo_seq = demo_seq, device=data.device)
+    elif type(data) == TargetPoseDemo:
+        scene_pc = downsample(data.scene_pc, voxel_size=voxel_size, coord_reduction=coord_reduction)
+        grasp_pc = downsample(data.grasp_pc, voxel_size=voxel_size, coord_reduction=coord_reduction)
+        target_poses = downsample(data.target_poses, voxel_size=voxel_size, coord_reduction=coord_reduction)
+        return TargetPoseDemo(scene_pc=scene_pc, grasp_pc=grasp_pc, target_poses=target_poses, device=data.device)
+    elif type(data) == PointCloud:
+        if data.is_empty():
+            return data
+        else:
+            return voxel_filter(pc=data, voxel_size=voxel_size, coord_reduction=coord_reduction)
+    elif type(data) == SE3:
+        return data
+    else:
+        raise TypeError(f"Unknown data type ({type(data)}) is given.")
+
+
+
+def apply_SE3(data, poses: SE3, apply_right: bool = False):
+    if data is None:
+        return None
+    elif type(data) == DemoSequence:
+        demo_seq = [apply_SE3(data=demo, poses=poses, apply_right=apply_right) for demo in data]
+        return DemoSequence(demo_seq = demo_seq, device=data.device)
+    elif type(data) == TargetPoseDemo:
+        scene_pc = apply_SE3(data=scene_pc, poses=poses, apply_right=apply_right)
+        grasp_pc = apply_SE3(data=grasp_pc, poses=poses, apply_right=apply_right)
+        target_poses = apply_SE3(data=target_poses, poses=poses, apply_right=apply_right)
+        return TargetPoseDemo(scene_pc=scene_pc, grasp_pc=grasp_pc, target_poses=target_poses, device=data.device)
+    elif type(data) == PointCloud:
+        if data.is_empty():
+            return data
+        else:
+            return data.transformed(poses)
+    elif type(data) == SE3:
+        return SE3.multiply(poses, data, apply_right=apply_right)
+    else:
+        raise TypeError(f"Unknown data type ({type(data)}) is given.")
+    
+
+
+def jitter_points(data, jitter_std: float):
+    if data is None:
+        return None
+    elif type(data) == DemoSequence:
+        demo_seq = [jitter_points(data=demo, jitter_std=jitter_std) for demo in data]
+        return DemoSequence(demo_seq = demo_seq, device=data.device)
+    elif type(data) == TargetPoseDemo:
+        scene_pc = jitter_points(data=scene_pc, jitter_std=jitter_std)
+        grasp_pc = jitter_points(data=grasp_pc, jitter_std=jitter_std)
+        target_poses = jitter_points(data=target_poses, jitter_std=jitter_std)
+        return TargetPoseDemo(scene_pc=scene_pc, grasp_pc=grasp_pc, target_poses=target_poses, device=data.device)
+    elif type(data) == PointCloud:
+        if data.is_empty():
+            return data
+        else:
+            points = data.points + torch.randn(*(data.points.shape), device=data.points.device, dtype=data.points.dtype) * jitter_std
+            return PointCloud(points=points, colors=data.colors)
+    elif type(data) == SE3:
+        return data
+    else:
+        raise TypeError(f"Unknown data type ({type(data)}) is given.")
+
+
+
+def jitter_colors(data, jitter_std: float, cutoff: bool = False):
+    if data is None:
+        return None
+    elif type(data) == DemoSequence:
+        demo_seq = [jitter_points(data=demo, jitter_std=jitter_std, cutoff=cutoff) for demo in data]
+        return DemoSequence(demo_seq = demo_seq, device=data.device)
+    elif type(data) == TargetPoseDemo:
+        scene_pc = jitter_points(data=scene_pc, jitter_std=jitter_std, cutoff=cutoff)
+        grasp_pc = jitter_points(data=grasp_pc, jitter_std=jitter_std, cutoff=cutoff)
+        target_poses = jitter_points(data=target_poses, jitter_std=jitter_std, cutoff=cutoff)
+        return TargetPoseDemo(scene_pc=scene_pc, grasp_pc=grasp_pc, target_poses=target_poses, device=data.device)
+    elif type(data) == PointCloud:
+        if data.is_empty():
+            return data
+        else:
+            colors = data.colors + torch.randn(*(data.colors.shape), device=data.colors.device, dtype=data.colors.dtype) * jitter_std
+            if cutoff:
+                colors = torch.where(colors > 1., torch.tensor(1., dtype=colors.dtype, device=colors.device), colors)
+                colors = torch.where(colors < 0., torch.tensor(0., dtype=colors.dtype, device=colors.device), colors)
+            return PointCloud(points=data.points, colors=colors)
+    elif type(data) == SE3:
+        return data
+    else:
+        raise TypeError(f"Unknown data type ({type(data)}) is given.")
+
+
+
+class EdfTransform(torch.nn.Module):
+    def __init__(self, supported_type):
+        super().__init__()
+        self.supported_type = supported_type
+
+    def forward(self, data):
+        if type(data) not in self.supported_type:
+            raise TypeError
+        return data
+
+
+
+class Rescale(EdfTransform):
+    def __init__(self, rescale_factor: float) -> None:
+        super().__init__(supported_type = [None, PointCloud, SE3, TargetPoseDemo, DemoSequence])
+        self.rescale_factor: float = rescale_factor
+
+    def forward(self, data):
+        data = super().forward(data)
+        return rescale(data=data, rescale_factor = self.rescale_factor)
+        
+
+    
+class NormalizeColor(EdfTransform):
+    def __init__(self, color_mean: torch.Tensor, color_std: torch.Tensor) -> None:
+        super().__init__(supported_type = [None, PointCloud, SE3, TargetPoseDemo, DemoSequence])
+        self.color_mean: torch.Tensor = color_mean
+        self.color_std: torch.Tensor = color_std
+
+    def forward(self, data):
+        data = super().forward(data)
+        return normalize_color(data=data, color_mean = self.color_mean, color_std = self.color_std)
+
+
+    
+class Downsample(EdfTransform):
+    def __init__(self, voxel_size: float, coord_reduction: str = "average") -> None:
+        super().__init__(supported_type = [None, PointCloud, SE3, TargetPoseDemo, DemoSequence])
+        self.voxel_size: float = voxel_size
+        self.coord_reduction: str = coord_reduction
+
+    def forward(self, data):
+        data = super().forward(data)
+        return downsample(data=data, voxel_size=self.voxel_size, coord_reduction=self.coord_reduction)
+
+
+    
+class ApplySE3(EdfTransform):
+    def __init__(self, poses: SE3, apply_right: bool = False) -> None:
+        super().__init__(supported_type = [None, PointCloud, SE3, TargetPoseDemo, DemoSequence])
+        self.poses = SE3(poses=poses.detach().clone())
+        self.apply_right = apply_right
+
+    def forward(self, data):
+        data = super().forward(data)
+        return apply_SE3(data=data, poses=self.poses, apply_right=self.apply_right)
+
+
+
+class PointJitter(EdfTransform):
+    def __init__(self, jitter_std: float) -> None:
+        super().__init__(supported_type = [None, PointCloud, SE3, TargetPoseDemo, DemoSequence])
+        self.jitter_std: float = jitter_std
+
+    def forward(self, data):
+        data = super().forward(data)
+        return jitter_points(data=data, jitter_std=self.jitter_std)
+    
+
+
+class ColorJitter(EdfTransform):
+    def __init__(self, jitter_std: float, cutoff: bool = False) -> None:
+        super().__init__(supported_type = [None, PointCloud, SE3, TargetPoseDemo, DemoSequence])
+        self.jitter_std: float = jitter_std
+        self.cutoff: bool = cutoff
+
+    def forward(self, data):
+        data = super().forward(data)
+        return jitter_colors(data=data, jitter_std=self.jitter_std, cutoff=self.cutoff)
---
+++ diffusion_edf/wigner.py
@@ -0,0 +1,205 @@
+from typing import List
+
+import torch
+from e3nn.o3._wigner import _Jd
+from e3nn.math._linalg import direct_sum
+from pytorch3d import transforms
+
+@torch.jit.script
+def quat_to_angle_fast(q): # >10 times faster than e3nn's quaternion_to_angle function
+    ang = transforms.matrix_to_euler_angles(transforms.quaternion_to_matrix(q), "YXY").T
+    return ang
+
+@torch.jit.script
+def _z_rot_mat(angle, l: int):
+    r"""
+    Create the matrix representation of a z-axis rotation by the given angle,
+    in the irrep l of dimension 2 * l + 1, in the basis of real centered
+    spherical harmonics (RC basis in rep_bases.py).
+    Note: this function is easy to use, but inefficient: only the entries
+    on the diagonal and anti-diagonal are non-zero, so explicitly constructing
+    this matrix is unnecessary.
+    """
+    #shape, device, dtype = angle.shape, angle.device, angle.dtype
+    # M = angle.new_zeros((*shape, 2 * l + 1, 2 * l + 1))
+    assert angle.dim() == 1
+    device, dtype = angle.device, angle.dtype
+    M = angle.new_zeros((len(angle), 2 * l + 1, 2 * l + 1))
+    inds = torch.arange(0, 2 * l + 1, 1, device=device)
+    reversed_inds = torch.arange(2 * l, -1, -1, device=device)
+    frequencies = torch.arange(l, -l - 1, -1, dtype=dtype, device=device)
+    # M[..., inds, reversed_inds] = torch.sin(frequencies * angle[..., None])
+    # M[..., inds, inds] = torch.cos(frequencies * angle[..., None])
+    M[:, inds, reversed_inds] = torch.sin(frequencies * angle[:, None])
+    M[:, inds, inds] = torch.cos(frequencies * angle[:, None])
+    return M
+
+@torch.jit.script
+def wigner_D(l: int, alpha, beta, gamma, J):
+    r"""Wigner D matrix representation of :math:`SO(3)`.
+    It satisfies the following properties:
+    * :math:`D(\text{identity rotation}) = \text{identity matrix}`
+    * :math:`D(R_1 \circ R_2) = D(R_1) \circ D(R_2)`
+    * :math:`D(R^{-1}) = D(R)^{-1} = D(R)^T`
+    * :math:`D(\text{rotation around Y axis})` has some property that allows us to use FFT in `ToS2Grid`
+    Code of this function has beed copied from `lie_learn <https://github.com/AMLab-Amsterdam/lie_learn>`_ made by Taco Cohen.
+    Parameters
+    ----------
+    l : int
+        :math:`l`
+    alpha : `torch.Tensor`
+        tensor of shape :math:`(...)`
+        Rotation :math:`\alpha` around Y axis, applied third.
+    beta : `torch.Tensor`
+        tensor of shape :math:`(...)`
+        Rotation :math:`\beta` around X axis, applied second.
+    gamma : `torch.Tensor`
+        tensor of shape :math:`(...)`
+        Rotation :math:`\gamma` around Y axis, applied first.
+    Returns
+    -------
+    `torch.Tensor`
+        tensor :math:`D^l(\alpha, \beta, \gamma)` of shape :math:`(2l+1, 2l+1)`
+    """
+    # if not l < len(_Jd):
+    #     raise NotImplementedError(f'wigner D maximum l implemented is {len(_Jd) - 1}, send us an email to ask for more')
+
+    #alpha, beta, gamma = torch.broadcast_tensors(alpha, beta, gamma)
+    #J = _Jd[l].to(dtype=alpha.dtype, device=alpha.device)
+
+    assert alpha.shape == beta.shape == gamma.shape # (Nt,)
+
+    Xa = _z_rot_mat(alpha, l)
+    Xb = _z_rot_mat(beta, l)
+    Xc = _z_rot_mat(gamma, l)
+    return Xa @ J @ Xb @ J @ Xc
+
+@torch.jit.script
+def D_from_angles_(ls: List[int], muls: List[int], Js: List[torch.Tensor], alpha, beta, gamma):
+    Ds = []
+    for l, mul, J in zip(ls, muls, Js):
+        D = wigner_D(l, alpha, beta, gamma, J)
+        for _ in range(mul):
+            Ds.append(D)
+    return Ds
+
+def D_from_angles(irreps, Js, alpha, beta, gamma):
+    ls = []
+    muls = []
+    for mul, ir in irreps:
+        ls.append(ir.l)
+        muls.append(mul)
+
+    Ds = D_from_angles_(ls, muls, Js, alpha, beta, gamma)
+    return direct_sum(*Ds)
+    #return torch.block_diag(*Ds)
+
+@torch.jit.script
+def D_from_quaternion_(ls: List[int], muls: List[int], Js: List[torch.Tensor], q):
+    angle = quat_to_angle_fast(q)
+    alpha, beta, gamma = angle[0], angle[1], angle[2]
+    return D_from_angles_(ls=ls, muls=muls, Js=Js, alpha=alpha, beta=beta, gamma=gamma)
+
+def D_from_quaternion(irreps, Js, q):
+    ls = []
+    muls = []
+    for mul, ir in irreps:
+        ls.append(ir.l)
+        muls.append(mul)
+
+    Ds = D_from_quaternion_(ls, muls, Js, q)
+    return direct_sum(*Ds)
+    #return torch.block_diag(*Ds)
+
+
+@torch.jit.script
+def transform_feature_slice(feature, alpha, beta, gamma, l: int, J):
+    assert feature.dim() == 2
+    feature = feature.reshape(feature.shape[-2], -1, 2*l+1) # (N_query, mul*(2l+1)) -> (N_query, mul, 2l+1)
+    D = wigner_D(l, alpha, beta, gamma, J) # (Nt, 2l+1, 2l+1)
+    feature_transformed = torch.einsum('tij,qmj->tqmi', D, feature) # (Nt, 2l+1, 2l+1) x (Nq, mul, 2l+1) -> (Nt, Nq, mul, 2l+1)
+    feature_transformed = feature_transformed.reshape(feature_transformed.shape[0], feature_transformed.shape[1], -1) # (Nt, N_query, mul*(2l+1))
+    return feature_transformed
+
+@torch.jit.script
+def transform_feature_(ls: List[int], feature_slices: List[torch.Tensor], Js: List[torch.Tensor], alpha, beta, gamma):
+    feature_transformed = []
+    for l, feature_slice, J in zip(ls, feature_slices, Js):
+        feature_transformed_slice = transform_feature_slice(feature_slice, alpha, beta, gamma, l, J)
+        feature_transformed.append(feature_transformed_slice)
+    
+    return torch.cat(feature_transformed, dim=-1)
+
+def transform_feature(irreps, feature, alpha, beta, gamma, Js):
+    ls = []
+    feature_slices = []
+    for (mul,ir), slice_ in zip(irreps, irreps.slices()):
+        ls.append(ir.l)
+        feature_slices.append(feature[..., slice_])
+
+    return transform_feature_(ls, feature_slices, Js, alpha, beta, gamma)
+
+@torch.jit.script
+def transform_feature_quat_(ls: List[int], feature_slices: List[torch.Tensor], Js: List[torch.Tensor], q: torch.Tensor):
+    q = transforms.standardize_quaternion(q / torch.norm(q, dim=-1, keepdim=True))
+    angle = quat_to_angle_fast(q)
+    alpha, beta, gamma = angle[0], angle[1], angle[2]
+    return transform_feature_(ls=ls, feature_slices=feature_slices, Js=Js, alpha=alpha, beta=beta, gamma=gamma)
+
+def transform_feature_quat(irreps, feature, q, Js):
+    ls = []
+    feature_slices = []
+    for (mul,ir), slice_ in zip(irreps, irreps.slices()):
+        ls.append(ir.l)
+        feature_slices.append(feature[..., slice_])
+
+    return transform_feature_quat_(ls, feature_slices, Js, q)
+
+
+class TransformFeatureQuaternion(torch.nn.Module):
+    def __init__(self, irreps):
+        super().__init__()
+        self.ls = tuple([ir.l for mul, ir in irreps])
+        self.slices = tuple([(slice_.start, slice_.stop) for slice_ in irreps.slices()])
+
+        self.Js = torch.nn.ParameterList([torch.nn.Parameter(_Jd[l].to(dtype = torch.float32), requires_grad=False) for l in self.ls])
+
+    @torch.jit.unused
+    def requires_grad_(self, requires_grad: bool):
+        return self
+
+    def forward(self, feature, q):
+        feature_slices = []
+        for slice_ in self.slices:
+            feature_slices.append(feature[..., slice_[0]:slice_[1]])
+        Js = [J.detach() for J in self.Js]
+
+        return transform_feature_quat_(ls=self.ls, feature_slices=feature_slices, Js=Js, q=q)
+
+# class TransformFeatureQuaternion(torch.nn.Module):
+#     def __init__(self, irreps):
+#         super().__init__()
+#         self.ls = tuple([ir.l for mul, ir in irreps])
+#         self.slices = tuple([(slice_.start, slice_.stop) for slice_ in irreps.slices()])
+#         for l in self.ls:
+#             self.register_buffer(f'J_{l}', _Jd[l].to(dtype = torch.float32), persistent=False)
+
+#     def forward(self, feature, q):
+#         feature_slices = []
+#         for slice_ in self.slices:
+#             feature_slices.append(feature[..., slice_[0]:slice_[1]])
+
+#         Js = tuple([self.__getattr__(f'J_{l}') for l in self.ls])
+
+#         return transform_feature_quat_(ls=self.ls, feature_slices=feature_slices, Js=Js, q=q)
+    
+#     @torch.jit.unused
+#     @staticmethod
+#     def traced(irreps, feature_shape, q_shape):
+#         if isinstance(feature_shape, int):
+#             feature_shape = (feature_shape,)
+#         if isinstance(q_shape, int):
+#             q_shape = (q_shape,)
+
+#         q_example = torch.randn(*q_shape,4)
+#         return torch.jit.trace(TransformFeatureQuaternion(irreps=irreps), example_inputs=(irreps.randn(*feature_shape,-1), transforms.standardize_quaternion(q_example/q_example.norm(dim=-1, keepdim=True))))
