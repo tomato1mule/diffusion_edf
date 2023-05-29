fixed score normalization but no much effect

diffusion_edf/multiscale_score_model.py
@@ -107,11 +107,11 @@ class MultiscaleScoreModel(torch.nn.Module):
                                                time = time)
         
         target_ang_score = target_ang_score * torch.sqrt(time[..., None])
-        target_lin_score = target_lin_score * torch.sqrt(time[..., None])
+        target_lin_score = target_lin_score * torch.sqrt(time[..., None]) * self.lin_mult
         ang_score_diff = target_ang_score - ang_score
         lin_score_diff = target_lin_score - lin_score
         ang_loss = torch.sum(torch.square(ang_score_diff), dim=-1).mean(dim=-1)
-        lin_loss = torch.sum(torch.square(lin_score_diff * self.lin_mult), dim=-1).mean(dim=-1)
+        lin_loss = torch.sum(torch.square(lin_score_diff), dim=-1).mean(dim=-1)
 
         loss = ang_loss + lin_loss
 

diffusion_edf/score_head.py
@@ -199,10 +199,10 @@ class ScoreModelHead(torch.nn.Module):
         qinv: torch.Tensor = transforms.quaternion_invert(q.unsqueeze(-2)) # (N_T, 1, 4)
         lin_vel = transforms.quaternion_apply(qinv, lin_vel) # (N_T, N_Q, 3)
         ang_spin = transforms.quaternion_apply(qinv, ang_spin) # (N_T, N_Q, 3)
-        ang_orbital = torch.cross(query_pcd.x.unsqueeze(0), lin_vel, dim=-1) # (N_T, N_Q, 3)
+        ang_orbital = torch.cross(query_pcd.x.unsqueeze(0) / self.lin_mult, lin_vel, dim=-1) # (N_T, N_Q, 3)
 
-        lin_vel = torch.einsum('q,tqi->ti', query_weight, lin_vel) / self.lin_mult # (N_T, 3)
-        ang_vel = (torch.einsum('q,tqi->ti', query_weight, ang_orbital) / self.lin_mult) \
-                +   (torch.einsum('q,tqi->ti', query_weight, ang_spin) / self.ang_mult) # (N_T, 3)
+        lin_vel = torch.einsum('q,tqi->ti', query_weight, lin_vel) # (N_T, 3)
+        ang_vel = (torch.einsum('q,tqi->ti', query_weight, ang_orbital)) \
+                +   (torch.einsum('q,tqi->ti', query_weight, ang_spin)) # (N_T, 3)
 
         return ang_vel, lin_vel