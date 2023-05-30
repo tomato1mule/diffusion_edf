d26d12f6f5b3aae713f7558f15cda292b7a8be0d
Hyunwoo Ryu <tomato1mule@gmail.com>
2023-05-30 18:34:33 +0900

    visualization renewed

--- diffusion_edf/visualize.py
+++ diffusion_edf/visualize.py
@@ -9,7 +9,7 @@ def visualize_pose(scene_pcd: PointCloud,
                    poses: SE3, 
                    query: Optional[Tuple[torch.Tensor, torch.Tensor]] = None, 
                    show_sample_points: bool = False, 
-                   point_size = 3.0):
+                   point_size = 3.0, width=800, height=800):
     
     grasp_pl = grasp_pcd.plotly(point_size=point_size, name="grasp")
     grasp_geometry = [grasp_pl]
@@ -27,7 +27,24 @@ def visualize_pose(scene_pcd: PointCloud,
     scene_pl = scene_pcd.plotly(point_size=point_size, name='scene')
     placement_geometry = [scene_pl]
     transformed_grasp_pcd = grasp_pcd.transformed(poses)
+
+    x_min = torch.min(scene_pcd.points[:,0]).item()
+    x_max = torch.max(scene_pcd.points[:,0]).item()
+    y_min = torch.min(scene_pcd.points[:,1]).item()
+    y_max = torch.max(scene_pcd.points[:,1]).item()
+    z_min = torch.min(scene_pcd.points[:,2]).item()
+    z_max = torch.max(scene_pcd.points[:,2]).item()
+
     for i in range(len(poses)):
+        mins_ = torch.min(transformed_grasp_pcd[i].points, dim=-2).values
+        maxs_ = torch.max(transformed_grasp_pcd[i].points, dim=-2).values
+
+        x_min = min(x_min, mins_[0].item())
+        x_max = max(x_max, maxs_[0].item())
+        y_min = min(y_min, mins_[1].item())
+        y_max = max(x_max, maxs_[1].item())
+        z_min = min(z_min, mins_[2].item())
+        z_max = max(x_max, maxs_[2].item())
         pose_pl = transformed_grasp_pcd[i].plotly(point_size=point_size, name=f'pose_{i}')
         placement_geometry.append(pose_pl)
     if show_sample_points:
@@ -64,9 +81,28 @@ def visualize_pose(scene_pcd: PointCloud,
         steps=steps
     )]
 
+    # x_min = min(fig_sample.data[0]['x'])
+    # x_max = max(fig_sample.data[0]['x'])
+    # y_min = min(fig_sample.data[0]['y'])
+    # y_max = max(fig_sample.data[0]['y'])
+    # z_min = min(fig_sample.data[0]['z'])
+    # z_max = max(fig_sample.data[0]['z'])
+    x_center = (x_max+x_min)/2
+    y_center = (y_max+y_min)/2
+    z_center = (z_max+z_min)/2
+    half_w = max(x_max-x_min, y_max-y_min, z_max-z_min)/2
+
+
     fig_sample.update_layout(
-        sliders=sliders
+        scene=dict(xaxis=dict(range=[x_center-half_w, x_center+half_w]),
+                   yaxis=dict(range=[y_center-half_w, y_center+half_w]),
+                   zaxis=dict(range=[z_center-half_w, z_center+half_w]),
+                   aspectmode='cube'),
+        sliders=sliders,
+        width=width,  # Adjust the width as needed
+        height=height,  # Adjust the height as needed
     )
+
 
     fig_sample.data[trace_dict[f'pose_0']].visible = True
 
