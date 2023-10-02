e4f325069b5786a5f10162df44dd92445168b586
pranaboy72 <junwoochang@yonsei.ac.kr>
2023-10-02 17:43:14 +0900

    typo

--- scripts/panda_real/agent_server.bash
+++ scripts/panda_real/agent_server.bash
@@ -9,7 +9,7 @@ nameserver_host_port="9090"
 
 python3 diffusion_edf/agent_server.py --configs-root-dir=$configs_root_dir \
                                       --server-name=$server_name \
-                                      --compile-score-model-head 
+                                      --compile-score-model-head \
                                       --nameserver-host-ip=$nameserver_host_ip \
                                       --nameserver-host-port=$nameserver_host_port \
                                     #   --init-nameserver
