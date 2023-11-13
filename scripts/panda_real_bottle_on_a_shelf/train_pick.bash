f8dd0b8054106371458133a628204e7e665650ef
Hyunwoo Ryu <tomato1mule@gmail.com>
2023-11-13 19:21:43 +0900

    bottle task

---
+++ scripts/panda_real_bottle_on_a_shelf/train_pick.bash
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "Train Low-resolution Score-matching Model"
+
+configs_root_dir="configs/panda_bowl/pick_lowres"
+train_configs_file="train_configs.yaml"
+task_configs_file="task_configs.yaml"
+
+PYTHONHASHSEED=0 python3 diffusion_edf/train.py --configs-root-dir=$configs_root_dir \
+                                                --train-configs-file=$train_configs_file \
+                                                --task-configs-file=$task_configs_file \
+                                                --log-name-postfix="Pick_LowRes_Panda_Bowl"
+
+echo "Train Super-resolution Score-matching Model"
+
+configs_root_dir="configs/panda_bowl/pick_highres"
+train_configs_file="train_configs.yaml"
+task_configs_file="task_configs.yaml"
+
+PYTHONHASHSEED=0 python3 diffusion_edf/train.py --configs-root-dir=$configs_root_dir \
+                                                --train-configs-file=$train_configs_file \
+                                                --task-configs-file=$task_configs_file \
+                                                --log-name-postfix="Pick_HiRes_Panda_Bowl"
+
+# echo "Train EBM Model"
+
+# configs_root_dir="configs/panda_bowl/pick_ebm"
+# train_configs_file="train_configs.yaml"
+# task_configs_file="task_configs.yaml"
+
+# PYTHONHASHSEED=0 python3 diffusion_edf/train.py --configs-root-dir=$configs_root_dir \
+#                                                 --train-configs-file=$train_configs_file \
+#                                                 --task-configs-file=$task_configs_file \
+#                                                 --log-name-postfix="Pick_EBM_Panda_Bowl"
+                                                
