941d6f299f56207fac1468da56c1551d84ce3f32
Hyunwoo Ryu <tomato1mule@gmail.com>
2023-11-14 17:01:32 +0900

    updated place_bottle

--- scripts/panda_real_bottle_on_a_shelf/train_pick.bash
+++ scripts/panda_real_bottle_on_a_shelf/train_pick.bash
@@ -22,14 +22,14 @@ PYTHONHASHSEED=0 python3 diffusion_edf/train.py --configs-root-dir=$configs_root
                                                 --task-configs-file=$task_configs_file \
                                                 --log-name-postfix="Pick_HiRes_Panda_Bottle"
 
-# echo "Train EBM Model"
+echo "Train EBM Model"
 
-# configs_root_dir="configs/panda_bottle/pick_ebm"
-# train_configs_file="train_configs.yaml"
-# task_configs_file="task_configs.yaml"
+configs_root_dir="configs/panda_bottle/pick_ebm"
+train_configs_file="train_configs.yaml"
+task_configs_file="task_configs.yaml"
 
-# PYTHONHASHSEED=0 python3 diffusion_edf/train.py --configs-root-dir=$configs_root_dir \
-#                                                 --train-configs-file=$train_configs_file \
-#                                                 --task-configs-file=$task_configs_file \
-#                                                 --log-name-postfix="Pick_EBM_Panda_Bottle"
+PYTHONHASHSEED=0 python3 diffusion_edf/train.py --configs-root-dir=$configs_root_dir \
+                                                --train-configs-file=$train_configs_file \
+                                                --task-configs-file=$task_configs_file \
+                                                --log-name-postfix="Pick_EBM_Panda_Bottle"
                                                 
